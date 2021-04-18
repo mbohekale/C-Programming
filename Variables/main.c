e7f3fc2ab90f4a54f364a5e0e059cd1e348fbf2d
mbohekale <mbohekale@yahoo.com>
2021-04-18 13:59:02 +0200

    appending files

---
+++ Variables/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char Myfirst[] = "Mboh Ekale";
+    int age =35;
+    printf("Hello world!\n");
+    printf("What is your name?\n");
+    printf("My Name is %s  \n",Myfirst);
+    printf("I am %d years old\n", age);
+    return 0;
+}
