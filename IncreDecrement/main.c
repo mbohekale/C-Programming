97855313ecbe52aeab072fec399a0334e72afe55
mbohekale <mbohekale@yahoo.com>
2021-04-24 19:56:30 +0200

    appending files

---
+++ IncreDecrement/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=1;
+    i++;
+    printf("Increment value is : %d\n ",i);
+    i--;
+    printf("Decrement value is : %d  ",i);
+
+    return 0;
+}
