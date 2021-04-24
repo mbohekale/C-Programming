11f167194c1d1050f60aa1126a05839e3b44eda7
mbohekale <mbohekale@yahoo.com>
2021-04-24 20:17:25 +0200

    appending files

---
+++ Conditions/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y;
+    x=5;
+    y=3;
+    if(x<y)
+        {
+        printf("x is less than y\n");
+        }
+        else
+        {
+        printf("x is greater than y\n");
+        }
+
+
+    return 0;
+}
