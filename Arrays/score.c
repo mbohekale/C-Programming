fe420f2975cb095501b9593cfec82ae3426bbf88
mbohekale <mbohekale@yahoo.com>
2021-04-26 21:45:22 +0200

    appending files

---
+++ Arrays/score.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+
+
+int main(void)
+{
+    int scores[3];
+	     scores[0]=10;
+         scores[1]=20;
+         scores[2]=30;
+   
+        printf("Average is :%i\n",(scores[0]+scores[1])/2);         
+}
