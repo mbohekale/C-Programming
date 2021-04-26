f995355dc8fee72727994f2c9220ffe4dd3e370c
mbohekale <mbohekale@yahoo.com>
2021-04-26 20:01:34 +0200

    appending files

---
+++ Arrays/Array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+
+
+int main(void)
+{
+	int duck[3];
+		duck[0]=12;
+        duck[1]=13;
+        duck[2]=14;
+        printf("%d,%d,%d\n",duck[0],duck[1],duck[2]);
+}
